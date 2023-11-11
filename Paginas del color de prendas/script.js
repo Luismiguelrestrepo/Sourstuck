97d93e8694b25d924747ebb75e6fea54a09ff6c5
Luismiguelrestrepo <felilconmifamilia@gmail.com>
2023-11-10 21:57:59 -0500

    Agrego al repositorio por primera vez

---
+++ Paginas del color de prendas/script.js
@@ -0,0 +1,18 @@
+document.getElementById("desplegablebtn").addEventListener("click", function() {
+  var opciones = document.getElementById("opciones");
+  if (opciones.style.display === "block") {
+    opciones.style.display = "none";  
+  } else {
+    opciones.style.display = "block";
+  }
+});
+
+document.getElementById("desplegablepantalon").addEventListener("click", function() {
+  var opcionespantalon = document.getElementById("opcionespantalon");
+  if (opcionespantalon.style.display === "block") {
+    opcionespantalon.style.display = "none";  
+  } else {
+    opcionespantalon.style.display = "block";
+  }
+});
+
